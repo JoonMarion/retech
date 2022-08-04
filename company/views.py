becd730e893cae1373e045f74da502345b27a155
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-08-04 23:52:00 +0000

    FEAT: func p/ formatar phone na aba serviços

--- company/views.py
+++ company/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from utils import *
 from .models import Company
 
 # Create your views here.
 def service(request):
     model = Company.objects.all()
+    for obj in model:
+        obj.phone = phone_formatted(obj.phone)
     return render(request, 'service.html', {'model': model})
--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ import uuid
 def upload_logo_formatter(instance, filename):
     ext = filename.split('.')[-1]
     return 'company_logo/logo-{}-{}'.format(instance.name, str(uuid.uuid1()) + '.' + ext)
+
+def phone_formatted(phone):
+    return f'({phone[0:2]}) {phone[2:7]}-{phone[7:]}'
