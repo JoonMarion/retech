8f898dc4d7274b5da51e29573459aed6b7a92958
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-08-05 01:15:20 -0300

    barra

--- retech/urls.py
+++ retech/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from home.views import healthcheck, home
+from home.views import home
 from information.views import information
 from company.views import service
 
@@ -10,15 +10,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # HOME
-    path('', home, name='home'),
+    path('/', home, name='home'),
 
     # INFORMATION
     path('information/', information, name='information'),
 
     # service
     path('service/', service, name='service'),
-    #Healthcheck
-    path('/', healthcheck, name='healthcheck'),
-
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
