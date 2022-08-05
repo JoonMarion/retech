339c9d5c8d7d7f508d008113941b469d4d0090c6
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-08-04 22:25:46 -0300

    fix: ajustando allowed hosts

--- retech/settings.py
+++ retech/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-35dlo9p@h$ib4zxg*r4=13w43xn^#9f+6%_y#n6%#^&h@swjcf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
