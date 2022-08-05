9171746db4d59d4d676ed7189f5511ee160ca8e4
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-08-05 01:49:38 +0000

    FEAT: funct search/paginação aba serviços

--- company/views.py
+++ company/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from utils import *
 from .models import Company
+from django.core.paginator import Paginator
 
 # Create your views here.
 def service(request):
-    model = Company.objects.all()
+    data = {}
+    search = request.GET.get('search')
+    if search:
+        model = Company.objects.filter(address__city__icontains=search) | Company.objects.filter(address__state__icontains=search)
+    else:
+        model = Company.objects.all()
     for obj in model:
         obj.phone = phone_formatted(obj.phone)
-    return render(request, 'service.html', {'model': model})
+    paginator = Paginator(model, 3)
+    page = request.GET.get('page')
+    data['db'] = paginator.get_page(page)
+    return render(request, 'service.html', data)
+
+def search_address_company(request):
+
+    return render(request, 'search_address_company.html')
