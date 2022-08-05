6634e3be3fee24053939017c9afa121ecf6447a7
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-08-05 01:12:52 -0300

    healthcheck

--- home/views.py
+++ home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
-
+@api_view(['GET'])
 def healthcheck(request):
     try:
         return Response({"message": 'ok'}, status=200)
