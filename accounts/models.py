f0273137caffd96db3306c62e9e86fd08f036d59
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-09-07 21:48:28 +0000

    feat: model|view  perfil user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from company.models import Address
 
 # Create your models here.
+class Perfil(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.SignUp.as_view(), name='signup'),
+    path('register/', views.SignUp, name='signup'),
 ]
