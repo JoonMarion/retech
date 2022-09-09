1f5c005511257626913855d330e9b452b31f9909
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-09-09 02:19:29 +0000

    fix: crsftoken form anuncio

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,8 @@
 from random import shuffle
 from django.shortcuts import render, redirect
 from .models import AnuncioVenda, AnuncioDoacao
+from company.models import Address
+from .models import AnuncioVenda, AnuncioDoacao
 
 
 # Create your views here.
@@ -14,11 +16,29 @@ def anunciar(request):
     contexto = {}
     if request.method == 'GET':
         if request.GET.get('tipoanuncio') == 'venda':
-            contexto = {'form': 'number">'}
+            contexto = {'inputtype': 'number">'}
         
         if request.GET.get('tipoanuncio') == 'doacao':
-            contexto = {'form': 'hidden'}
+            contexto = {'inputtype': 'hidden'}
 
     if request.method == 'POST':
-        pass
+        novo_endereco = Address(
+            city=request.POST['cidade'].capitalize(),
+            state=request.POST['estado'].upper(),
+        )
+        novo_endereco.save()
+        if request.POST['tipoanuncio'] == 'venda':
+            novo_anuncio = AnuncioVenda(
+                title=request.POST['titulo'],
+                description=request.POST['descricao'],
+                price=request.POST['preco'],
+                address=novo_endereco,
+                user=request.user,
+            )
+            novo_anuncio.save()
+            return redirect('/marketplace')
+
+
+
+
     return render(request, 'anunciar.html', {'contexto': contexto})
